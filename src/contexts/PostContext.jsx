673f1fecb924fef645539c1e6dc7d65808eb0cd1
jochen <jochen@byte23.net>
2024-11-20 10:21:13 +0100

    primitive postContext and savePost functions

--- src/contexts/PostContext.jsx
+++ src/contexts/PostContext.jsx
@@ -1,1 +1,26 @@
 // Context API pattern: Provides global state and functions for managing posts.
+
+import { createContext, useState } from 'react';
+// import { getPosts } from " "
+
+export const PostsContext = createContext(null);
+
+export function PostsContextProvider() {
+    const [posts, setPosts] = useState();
+
+    function savePost(post) {
+        const found = posts.findIndex((el) => el.id === post.id);
+        if ((found = undefined)) {
+            const newPosts = [...posts];
+            newPosts.push(post);
+        }
+    }
+
+    savePost();
+
+    return (
+        <PostsContext.Provider value={{ posts, setPosts }}>
+            {children}
+        </PostsContext.Provider>
+    );
+}
