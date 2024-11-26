e250c7ecbfbf8a5faf0e67d31652064323a66f58
Yevhen <eugene.shust@gmail.com>
2024-11-27 00:42:29 +0100

    Loaded profile when accessToken exist

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,41 @@
 import { Outlet } from 'react-router-dom';
 import { Header, Footer } from '../components';
+import { useAuth } from '../hooks';
+import { getToken } from '../utils';
+import { useEffect } from 'react';
+import { getProfile } from '../services';
 
 export const MainLayout = () => {
+    const { updateProfile } = useAuth();
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                console.log('step1');
+                const token = getToken();
+                console.log(token);
+                if (token.length === 0) return;
+
+                const data = await getProfile();
+                console.log('MainLayout.fetchProfile', data);
+                updateProfile(data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        getData();
+
+        return () => {
+            console.log('Cleanup function ran');
+        };
+    }, []);
+
     return (
         <>
-            <div className="max-w-screen-xl mx-auto">
+            <div className="min-h-screen flex flex-col max-w-screen-xl mx-auto">
                 <Header />
-                <main className="container mx-auto">
+                <main className="flex-grow container mx-auto">
                     <Outlet />
                 </main>
                 <Footer />
