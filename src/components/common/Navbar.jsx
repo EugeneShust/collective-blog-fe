b13b4ee24c9f4857b1648cee6ae28cd1e6afbc21
Yevhen <eugene.shust@gmail.com>
2024-11-27 00:52:52 +0100

    Updated Navbar for auth

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../hooks';
 
 export function Navbar() {
+    const { user, logout } = useAuth();
     return (
         <nav>
             <div className="navbar bg-[#374151]">
@@ -9,6 +11,7 @@ export function Navbar() {
                         <li className="btn btn-ghost text-xl">
                             the awesome blogpost page
                         </li>
+                    </Link>
                 </div>
                 <div className="flex-none gap-4">
                     <Link to="/create-post">
@@ -16,31 +19,41 @@ export function Navbar() {
                             New blogpost
                         </div>
                     </Link>
-                    <div className="dropdown dropdown-end">
-                        <div
-                            tabIndex={0}
-                            role="button"
-                            className="btn btn-ghost btn-circle avatar"
-                        >
-                            <div className="w-10 rounded-full">
-                                <img
-                                    alt="Profile"
-                                    src="../src/assets/images/avatar.jpg"
-                                />
+                    {!user ? (
+                        <Link to="/login" className="text-xl">
+                            Login
+                        </Link>
+                    ) : (
+                        <div className="dropdown dropdown-end">
+                            <div
+                                tabIndex={0}
+                                role="button"
+                                className="btn btn-ghost btn-circle avatar"
+                            >
+                                <div className="w-10 rounded-full">
+                                    <img
+                                        alt="Profile"
+                                        src="../src/assets/images/avatar.jpg"
+                                    />
+                                </div>
                             </div>
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                            >
+                                <li className="text-right text-xl">
+                                    <Link className="block" to="/profile">
+                                        Profile
+                                    </Link>
+                                </li>
+                                <li className="text-right text-xl">
+                                    <Link className="block" onClick={logout}>
+                                        Logout
+                                    </Link>
+                                </li>
+                            </ul>
                         </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-                        >
-                            <li>
-                                <a className="justify-between">Profile</a>
-                            </li>
-                            <li>
-                                <a>Logout</a>
-                            </li>
-                        </ul>
-                    </div>
+                    )}
                 </div>
             </div>
         </nav>
