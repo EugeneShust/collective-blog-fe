df00b4a277bd30060a82b2eba4566f1939f6bd22
Yevhen <eugene.shust@gmail.com>
2024-11-28 13:05:34 +0100

    Added BlogPostComponent

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ export { Navbar } from './common/Navbar';
 export { MainLayout } from '../layouts/MainLayout';
 export { Footer } from './common/Footer';
 export { Alert } from './common/Alert';
-export { Post } from './posts/Post';
+export { BlogPost } from './posts/BlogPost';
 export { Button } from './common/Button';
 export { LoginForm } from './auth/LoginForm';
 export { SignUpForm } from './auth/SignUpForm';
