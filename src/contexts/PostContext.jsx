2f53d377093cc48106a821375b225c6648213d12
Yevhen <eugene.shust@gmail.com>
2024-11-28 13:27:35 +0100

    Fixed postContext.

--- src/contexts/PostContext.jsx
+++ src/contexts/PostContext.jsx
@@ -1,22 +1,34 @@
 // Context API pattern: Provides global state and functions for managing posts.
 
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const PostsContext = createContext(null);
 
 export const PostsProvider = ({ children }) => {
     const [posts, setPosts] = useState([]);
 
-    function AddOrUpdatePost(post) {
-        const newPosts = [...posts];
-        const index = posts.findIndex((el) => el.id === post.id);
+    function addOrUpdatePost(post) {
+        // It is necessary to use the functional form (setState((prev) => {const newState = [...prevState]; return newState}) because:
+        //   - It isolates the state updating from external changes.
+        //   - It solves the problem of asynchrony Setstate.
 
-        if (index === -1) newPosts.push(post);
-        else newPosts[index] = post;
+        setPosts((prevPosts) => {
+            const newPosts = [...prevPosts];
+            const index = newPosts.findIndex((el) => el.id === post.id);
+
+            if (index === -1) newPosts.push(post);
+            else newPosts[index] = post;
+
+            return newPosts;
+        });
     }
 
+    useEffect(() => {
+        console.log('Updated posts:', posts); // Лог оновленого стану
+    }, [posts]);
+
     return (
-        <PostsContext.Provider value={{ posts, AddOrUpdatePost }}>
+        <PostsContext.Provider value={{ posts, addOrUpdatePost }}>
             {children}
         </PostsContext.Provider>
     );
